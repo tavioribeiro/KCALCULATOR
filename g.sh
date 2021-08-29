3824ae7e671f15434f9cb7575a5f23ab5d7e1a5d
tavioribeiro <otavio123ribeiro@gmail.com>
2021-08-29 19:17:10 -0300

    test shell auto deploy

--- g.sh
+++ g.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo "Enter your message"
+read message
 git add .
-git commit -m "v3.4.1"
-git push
+git commit -m"${message}"
+if [ -n "$(git status - porcelain)" ];
+then
+ echo "IT IS CLEAN"
+else
+ git status
+ echo "Pushing data to remote server!!!"
+ git push -u origin master
+fi
--- g1.sh
+++ g1.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo Hello World
+
+gnome-terminal
+
+./g.sh
